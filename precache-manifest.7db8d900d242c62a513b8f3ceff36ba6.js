2958f7be2f6cc918f9553c61afbc75de7fd32a2f
Vladimir <ko4etov.volodya@yandex.ru>
2019-09-29 17:11:58 +0500

    Updates

---
+++ precache-manifest.7db8d900d242c62a513b8f3ceff36ba6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadfd1e6c556a23b9b66552ac6eeb5a8",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "c5fc273c48ed183e5f5d",
+    "url": "/myapp/static/css/main.fe2cb4f4.chunk.css"
+  },
+  {
+    "revision": "f2b3d9a866198dbe380e",
+    "url": "/myapp/static/js/2.d29f2b6c.chunk.js"
+  },
+  {
+    "revision": "c5fc273c48ed183e5f5d",
+    "url": "/myapp/static/js/main.a6d6c6c3.chunk.js"
+  },
+  {
+    "revision": "0c9ff3fd583a091d14c7",
+    "url": "/myapp/static/js/runtime-main.94f3aa5b.js"
+  }
+]);
